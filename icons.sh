63ea935571136e4372fae70a0028400701d61bf2
Max Lang <17551908+just-max@users.noreply.github.com>
2022-03-16 00:33:49 +0100

    update all paths to be relative

--- icons.sh
+++ icons.sh
@@ -12,18 +12,18 @@ convert ./icon-32.png ./icon-16.png ./favicon.ico
 convert icon-140.png -bordercolor transparent -border 20 apple-touch-icon.png
 
 cat <<'EOF'
-<link rel="manifest" href="/manifest.webmanifest">
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/icon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="manifest" href="manifest.webmanifest">
+<link rel="icon" href="favicon.ico" sizes="any">
+<link rel="icon" href="icon.svg" type="image/svg+xml">
+<link rel="apple-touch-icon" href="apple-touch-icon.png">
 EOF
 
 cat > ./manifest.webmanifest <<'EOF'
 {
   "name": "Variable Font Demo",
   "icons": [
-    { "src": "/icon-192.png", "type": "image/png", "sizes": "192x192" },
-    { "src": "/icon-512.png", "type": "image/png", "sizes": "512x512" }
+    { "src": "icon-192.png", "type": "image/png", "sizes": "192x192" },
+    { "src": "icon-512.png", "type": "image/png", "sizes": "512x512" }
   ]
 }
 EOF
